2cbd44871b0b9f1d19d7995be5b22513cd0e5e16
Isidora Katara <50339659+isidorakatara@users.noreply.github.com>
2024-08-05 17:36:13 +0100

    additional areas

--- subareas/NPminke/areas.R
+++ subareas/NPminke/areas.R
@@ -1,3 +1,29 @@
+library(sf)
+library("ggplot2")
+theme_set(theme_bw())
+sf::sf_use_s2(FALSE)
+
+#### mapping function ####
+mappingf <- function(x){
+  
+  library("ggplot2")
+  theme_set(theme_bw())
+  
+  areas_bbox <- st_bbox(x)
+  x <- cbind(x, st_coordinates(st_centroid(x)))
+  y <- ggplot() +
+    geom_sf(data = x, fill = "lightblue", alpha=0.3) + 
+    geom_text(data = x, aes(X, Y, label = subareas), size = 2) +
+    coord_sf(xlim = c(areas_bbox$xmin, areas_bbox$xmax), ylim = c(areas_bbox$ymin, areas_bbox$ymax), expand = T)
+  print(y)
+  return(y)
+}
+
+#### polygons of oceans ####
+oceanshp <- "C:/Users/IsidoraKatara/OneDrive - International Whaling Commission/thirdpartydata/marineregions/Global Oceans and Seas/goas_v01.shp"
+oceanpoly <- st_read(oceanshp)
+
+#### subareas as WKT ####
 subareas = c("1W","1E","2C", "2R", "3", "4", "5", "6W", "6E", "7CS", "7WR", "7E", "7CN", "8", "9","10E","11", "10W", "12SW","12NE", "9N", "13")
 str=c("POLYGON((115 25 ,127 25 ,127 33 ,115 33 ,115 25))", 
 "POLYGON((127 25 ,130 25 ,130 33 ,127 33 ,127 25 ))", 
@@ -22,8 +48,40 @@ str=c("POLYGON((115 25 ,127 25 ,127 33 ,115 33 ,115 25))",
 "POLYGON((157 52,170 52,170 60,162 60,160 55,157 55,157 52))",
 "POLYGON((170 25,180 25,180 60,170 60,170 25))")
 
-library(sf)
+#### US west Coast ####
+subareasUSWC = c("US West Coast")
+strUSWC = c("POLYGON((-130 45.8,-124 47, -115 33.5, -117.5 33.5, -119 32, -123.5 30, -126.5 32, -130 40, -130 45.8))")
+areasUSWC <- st_as_sf(data.frame(subareas = subareasUSWC, geometry=strUSWC),wkt="geometry", crs = 4326)
+areasUSWC2 <- st_intersection(st_make_valid(areasUSWC), oceanpoly)
+plot(areasUSWC2["subareas"])
 
+#### eastern Bering Sea shelf ####
+subareasEBSS = c("eastern Bering Sea shelf")
+strEBSS = c("POLYGON((-179.9 60,-177 62,-161 56,-168 53,-179.9 60))")
+areasEBSS <- st_as_sf(data.frame(subareas = subareasEBSS, geometry=strEBSS),wkt="geometry", crs = 4326)
+areasEBSS2 <- st_intersection(st_make_valid(areasEBSS), oceanpoly)
+plot(areasEBSS2["subareas"])
+
+#### Aleutian Islands and the Alaska Peninsula ####
+subareasAIAP = c("Aleutian Islands and the Alaska Peninsula")
+strAIAP = c("POLYGON((-178 51,-178 52,-165 54,-155 57,-149 60,-149.5 59, -150 58.5, -150 58, -155 55, -160 53.8, -170 51.5, -178 51))")
+areasAIAP <- st_as_sf(data.frame(subareas = subareasAIAP, geometry=strAIAP),wkt="geometry", crs = 4326)
+areasAIAP2 <- st_intersection(st_make_valid(areasAIAP), oceanpoly)
+plot(areasAIAP2["subareas"])
+
+#### 7WR subarea divided ####
+subareas7wr = c("7WRS","7WRN")
+str7wr = c("POLYGON((142 35,147 35,147 40,143.25 40,142 35))","POLYGON((143.25 40,143 41,147 42.75,147 40,143.25 40))")
+areas7wr <- st_as_sf(data.frame(subareas = subareas7wr, geometry=str7wr),wkt="geometry", crs = 4326)
+
+ggplot() +
+  geom_sf(data = x, fill = x$subareas) + 
+  geom_sf(data = areas7wr, fill = "grey") + 
+  geom_text(data = x, aes(X, Y, label = subareas), size = 2) +
+  coord_sf(xlim = c(areas_bbox$xmin, areas_bbox$xmax), ylim = c(areas_bbox$ymin, areas_bbox$ymax), expand = T)
+
+
+########
 testing_wkt <- st_as_sf(data.frame(g=str),wkt="g", crs = "+init=epsg:4326")
 
 areas1 <- st_as_sf(data.frame(subareas = subareas, g=str),wkt="g", crs = 4326)#"+init=epsg:4326"
@@ -32,23 +90,14 @@ plot(areas1, axes = TRUE)
 
 areas_bbox <- st_bbox(areas1)
 
-oceanshp <- "Z:\\Documents\\statistics\\thirdpartydata\\marineregions\\Global Oceans and Seas\\goas_v01.shp"
-oceanpoly <- st_read(oceanshp)
-
-library("ggplot2")
-theme_set(theme_bw())
-
 ggplot(data = oceanpoly) +
   geom_sf() +
   geom_sf(data = areas1, fill = NA) + 
   geom_text(data = areas1, aes(X, Y, label = subareas), size = 5) +
   coord_sf(xlim = c(areas_bbox$xmin, areas_bbox$xmax), ylim = c(areas_bbox$ymin, areas_bbox$ymax), expand = T)
 
-sf::sf_use_s2(FALSE)
-
 areas2 <- st_intersection(st_make_valid(areas1), oceanpoly)
 plot(areas2["subareas"])
-
 
 areas2[areas2$subareas == "2C","g"]
 
@@ -63,10 +112,48 @@ st_write(areas2, "C:\\Users\\IsidoraKatara\\OneDrive - International Whaling Com
 
 correctareas <- st_read("C:\\Users\\IsidoraKatara\\OneDrive - International Whaling Commission\\minke whales\\NP\\subareas 102025.shp")
 
-areas_bbox <- st_bbox(correctareas)
-correctareas <- cbind(correctareas, st_coordinates(st_centroid(correctareas)))
-ggplot(data = correctareas) +
-  geom_sf() +
-  geom_sf(data = correctareas, fill = correctareas$subareas) + 
-  geom_text(data = correctareas, aes(X.1, Y.1, label = subareas), size = 2) +
+mappingf(x=NPareas)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+NPareas <- st_read("C:/Users/IsidoraKatara/OneDrive - International Whaling Commission/Abundance-Tabs&Refs/Extras/mapping subareas/NP-Mi/subareas 18032022.shp")
+
+areas_bbox <- st_bbox(NPareas)
+NPareasentroid <- cbind(NPareas, st_coordinates(st_centroid(NPareas)))
+ggplot() +
+  geom_sf(data = NPareas, fill = NPareas$subareas) + 
+  geom_text(data = NPareasentroid, aes(X, Y, label = subareas), size = 2) +
   coord_sf(xlim = c(areas_bbox$xmin, areas_bbox$xmax), ylim = c(areas_bbox$ymin, areas_bbox$ymax), expand = T)
+
+lookup1 <- data.frame(subareas = c("2C", "2R", "3", "4", "5", "6E", "6W", "7CN", "7E", "7WR", "7CS", "8", "10E", "10W", "13", "11", "12SW", "12NE", "1W", "1E"), bigareas = c("2", "2", "3", "4", "5", "6", "6", "7", "7", "7", "7", "8", "10", "10", "13", "11", "12", "12", "1", "1"))
+
+NPareas1 <- merge(NPareas,lookup1, by="subareas")
+nc_dissolve <- NPareas1 %>% group_by(bigareas) %>% summarize() %>% rename(subareas = bigareas)
+areas_bbox <- st_bbox(nc_dissolve)
+nc_dissolve <- cbind(nc_dissolve, st_coordinates(st_centroid(nc_dissolve)))
+ggplot(data = nc_dissolve) +
+  geom_sf(data = nc_dissolve, fill = nc_dissolve$bigareas) + 
+  geom_text(data = nc_dissolve, aes(X, Y, label = bigareas), size = 2) +
+  coord_sf(xlim = c(areas_bbox$xmin, areas_bbox$xmax), ylim = c(areas_bbox$ymin, areas_bbox$ymax), expand = T)
+
+#### 9N and 9S ####
+subareas9s = c("9-S")
+str9s = c("POLYGON((157 35,170 35,170 43,157 43,157 35))")
+areas9s <- st_as_sf(data.frame(subareas = subareas9s, geometry=str9s),wkt="geometry", crs = 4326) 
+areas9n <- st_difference(NPareas[NPareas$subareas=="9",], areas9s) %>% mutate(subareas = "9-N")
+JARPN_9NS <- dplyr::bind_rows(list(areas9s,areas9n)) %>% select(subareas, geometry)
+ggplot() +
+  geom_sf(data = x, fill = x$subareas) + 
+  geom_sf(data = JARPN_9NS, fill = "grey") + 
+  geom_text(data = x, aes(X, Y, label = subareas), size = 2) +
+  coord_sf(xlim = c(areas_bbox$xmin, areas_bbox$xmax), ylim = c(areas_bbox$ymin, areas_bbox$ymax), expand = T)
+
+#add the Hawaian EEZ
+eezs <- st_read("C:/Users/IsidoraKatara/OneDrive - International Whaling Commission/thirdpartydata/EEZ_land_union_v3_202003/EEZ_Land_v3_202030.shp")
+Hawaii <- eezs %>% filter(UNION == "Hawaiian EEZ") %>% rename(subarea = UNION) %>%
+  select(subarea,geometry)
+
+#### final output ####
+output <- dplyr::bind_rows(list(NPareas,nc_dissolve,areas7wr,JARPN_9NS,Hawaii,areasUSWC2,areasEBSS2,areasAIAP2)) %>% 
+  select(subareas, geometry) %>% 
+  mutate(large_area = "NP", species = "Mi")
+mappingf(output)
+write_sf(output, "C:/Users/IsidoraKatara/OneDrive - International Whaling Commission/Abundance-Tabs&Refs/Extras/mapping subareas/NP-Mi/Misubareas-10072024.shp")
